e1b1d3d791ae8293a57d3813ee8a37218be1acc5
Ajoy10 <achu.ajoy10@gmail.com>
2024-08-28 14:48:15 +0530

    Replaced dummy images on /api/v1/posts

--- server/posts/posts.router.js
+++ server/posts/posts.router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { fetchPosts } = require('./posts.service');
+const { fetchPosts, fetchImagesForPosts } = require('./posts.service');
 const { fetchUserById } = require('../users/users.service');
 
 const router = express.Router();
@@ -7,21 +7,7 @@ const router = express.Router();
 router.get('/', async (req, res) => {
   const posts = await fetchPosts();
 
-  const postsWithImages = posts.reduce((acc, post) => {
-    // TODO use this route to fetch photos for each post
-    // axios.get(`https://jsonplaceholder.typicode.com/albums/${post.id}/photos`);
-    return [
-      ...acc,
-      {
-        ...post,
-        images: [
-          { url: 'https://picsum.photos/200/300' },
-          { url: 'https://picsum.photos/200/300' },
-          { url: 'https://picsum.photos/200/300' },
-        ],
-      },
-    ];
-  }, []);
+  const postsWithImages = await fetchImagesForPosts(posts);
 
   res.json(postsWithImages);
 });
--- server/posts/posts.service.js
+++ server/posts/posts.service.js
@@ -23,4 +23,32 @@ async function fetchPosts(params) {
   return posts;
 }
 
-module.exports = { fetchPosts };
+/**
+ * Fetches images for each post from a remote API and return the list of posts with images attached.
+ * @async
+ * @param {Array} [posts] - The list of posts for fetching images.
+ * @returns {Promise<Array>} - A promise that resolves to an array of posts.
+ */
+async function fetchImagesForPosts(posts) {
+  const result = await Promise.all(
+    posts.map(async post => {
+      try {
+        const res = await axios.get(
+          `https://jsonplaceholder.typicode.com/albums/${post.id}/photos`,
+        );
+        post.images = res.data.map(img => {
+          return { url: img.url };
+        });
+        return post;
+      } catch (err) {
+        console.error(err);
+        post.images = [];
+        return post;
+      }
+    }),
+  );
+
+  return result;
+}
+
+module.exports = { fetchPosts, fetchImagesForPosts };
