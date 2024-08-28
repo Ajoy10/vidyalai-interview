2be7a4184d7d06f3877fc30244879e6d91b83c70
Ajoy10 <achu.ajoy10@gmail.com>
2024-08-28 15:25:32 +0530

    Implemented functionality to load more posts

--- server/posts/posts.router.js
+++ server/posts/posts.router.js
@@ -4,12 +4,18 @@ const { fetchUserById } = require('../users/users.service');
 
 const router = express.Router();
 
+const totalNoOfPosts = 100;
+
 router.get('/', async (req, res) => {
-  const posts = await fetchPosts();
+  const { start = 0, limit = 10 } = req.query;
+  const posts = await fetchPosts({ start, limit });
 
   const postsWithImages = await fetchImagesForPosts(posts);
 
-  res.json(postsWithImages);
+  res.json({
+    posts: postsWithImages,
+    remaining: totalNoOfPosts - (Number(start) + posts.length),
+  });
 });
 
 module.exports = router;
